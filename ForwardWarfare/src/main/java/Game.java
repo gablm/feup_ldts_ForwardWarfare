a28be75ed2d694952a06d06802ab60b3788420ba
FrozenHart <filipe.neves.esteves.4@gmail.com>
2023-11-18 01:16:28 +0000

    Font Implemented/Test Map

--- ForwardWarfare/src/main/java/Game.java
+++ ForwardWarfare/src/main/java/Game.java
@@ -1,3 +1,4 @@
+import com.googlecode.lanterna.SGR;
 import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
@@ -20,7 +21,7 @@ public class Game {
     public Game() {}
 
     private Font LoadExternalFonts() throws IOException, FontFormatException, URISyntaxException {
-        URL resource = getClass().getClassLoader().getResource("square.ttf");
+        URL resource = getClass().getClassLoader().getResource("tanks.ttf");
         assert resource != null;
         File fontFile = new File(resource.toURI());
         Font font =  Font.createFont(Font.TRUETYPE_FONT, fontFile);
@@ -28,7 +29,7 @@ public class Game {
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         ge.registerFont(font);
 
-        return font.deriveFont(Font.PLAIN, 3);
+        return font.deriveFont(Font.PLAIN, 60);
     }
     public void run() throws IOException, URISyntaxException, FontFormatException {
         DefaultTerminalFactory factory = new DefaultTerminalFactory();
@@ -36,7 +37,7 @@ public class Game {
         AWTTerminalFontConfiguration fontConfig = AWTTerminalFontConfiguration.newInstance(LoadExternalFonts());
         factory.setTerminalEmulatorFontConfiguration(fontConfig);
         factory.setForceAWTOverSwing(true);
-        factory.setInitialTerminalSize(new TerminalSize(256, 160 + 45));
+        factory.setInitialTerminalSize(new TerminalSize(11,11 ));
 
         Terminal terminal = factory.createTerminal();
         screen = new TerminalScreen(terminal);
@@ -48,7 +49,7 @@ public class Game {
     }
 
     private void DrawTiles(TextGraphics graphics) throws IOException {
-        boolean change = true;
+        /*boolean change = true;
         for (int j = 0; j < 160; j += 16) {
             change = !change;
             for (int i = 0; i < 256; i += 16) {
@@ -56,7 +57,87 @@ public class Game {
                 change = !change;
                 graphics.drawRectangle(new TerminalPosition(i, j), new TerminalSize(16, 16), ' ');
             }
-        }
+        }*/
+
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FFFFFF"));
+        /*
+        graphics.putString(new TerminalPosition(0,0), "Tanks $#>+ ");
+        graphics.putString(new TerminalPosition(0,1), "Person (_@ ");
+        graphics.putString(new TerminalPosition(0,2), "Plane %& ");
+        graphics.putString(new TerminalPosition(0,3), "Helly /[ ");
+        graphics.putString(new TerminalPosition(0,4), "Boat <)' ");
+        graphics.putString(new TerminalPosition(0,5), "Submarine = ");
+        graphics.putString(new TerminalPosition(0,6), "Tiles ~}{`]|;");
+        graphics.putString(new TerminalPosition(0,7), "Borders ^* ");
+        graphics.putString(new TerminalPosition(0,8), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        graphics.putString(new TerminalPosition(0,9), "abcdefghijklmnop");
+        graphics.putString(new TerminalPosition(0,10), ".,!?-:");
+        */
+        /*
+        graphics.setBackgroundColor(TextColor.ANSI.BLUE);
+        graphics.putString(new TerminalPosition(0,0), "~~~~~~~~~~~");
+        graphics.putString(new TerminalPosition(0,1), "~");
+        graphics.putString(new TerminalPosition(10,1), "~");
+        graphics.putString(new TerminalPosition(0,2), "~");
+        graphics.putString(new TerminalPosition(7,2), "~~~~");
+        graphics.putString(new TerminalPosition(0,3), "~~~~");
+        graphics.putString(new TerminalPosition(10,3), "~");
+        graphics.putString(new TerminalPosition(0,4), "~");
+        graphics.putString(new TerminalPosition(10,4), "~");
+        graphics.putString(new TerminalPosition(0,5), "~");
+        graphics.putString(new TerminalPosition(10,5), "~");
+        graphics.putString(new TerminalPosition(0,6), "~");
+        graphics.putString(new TerminalPosition(8,6), "~~~");
+        graphics.putString(new TerminalPosition(0,7), "~~~");
+        graphics.putString(new TerminalPosition(10,7), "~");
+        graphics.putString(new TerminalPosition(0,8), "~");
+        graphics.putString(new TerminalPosition(10,8), "~");
+        graphics.putString(new TerminalPosition(0,9), "~");
+        graphics.putString(new TerminalPosition(10,9), "~");
+        graphics.putString(new TerminalPosition(0,10), "~~~~~~~~~~~");
+        graphics.setForegroundColor(TextColor.ANSI.CYAN);
+        graphics.putString(new TerminalPosition(4,3), ";");
+        graphics.setForegroundColor(TextColor.ANSI.WHITE);
+        graphics.setBackgroundColor(new TextColor.RGB(128,128,128));
+        graphics.putString(new TerminalPosition(1,5), "}}}}");
+        graphics.putString(new TerminalPosition(6,5), "}}}}");
+        graphics.setBackgroundColor(TextColor.ANSI.GREEN);
+        graphics.putString(new TerminalPosition(1,1), "|");
+        graphics.putString(new TerminalPosition(4,1), "|||");
+        graphics.putString(new TerminalPosition(8,1), "||");
+        graphics.putString(new TerminalPosition(1,2), "||||||");
+        graphics.putString(new TerminalPosition(5,3), "|||");
+        graphics.putString(new TerminalPosition(9,3), "|");
+        graphics.putString(new TerminalPosition(1,4), "||");
+        graphics.putString(new TerminalPosition(4,4), "||||||");
+        graphics.putString(new TerminalPosition(5,5), "|");
+        graphics.putString(new TerminalPosition(1,6), "|");
+        graphics.putString(new TerminalPosition(3,6), "|||||");
+        graphics.putString(new TerminalPosition(3,7), "|||||||");
+        graphics.putString(new TerminalPosition(1,8), "|||");
+        graphics.putString(new TerminalPosition(5,8), "|||||");
+        graphics.putString(new TerminalPosition(1,9), "|");
+        graphics.putString(new TerminalPosition(3,9), "|||||");
+        graphics.putString(new TerminalPosition(9,9), "|");
+        graphics.setForegroundColor(TextColor.ANSI.BLACK);
+        graphics.putString(new TerminalPosition(3,1), "#");
+        graphics.putString(new TerminalPosition(2,1), ";");
+        graphics.setForegroundColor(TextColor.ANSI.BLUE_BRIGHT);
+        graphics.putString(new TerminalPosition(7,1), ";");
+        graphics.setForegroundColor(TextColor.ANSI.MAGENTA);
+        graphics.putString(new TerminalPosition(8,3), ";");
+        graphics.setForegroundColor(TextColor.ANSI.YELLOW);
+        graphics.putString(new TerminalPosition(3,4), ";");
+        graphics.setForegroundColor(TextColor.ANSI.YELLOW);
+        graphics.putString(new TerminalPosition(2,6), ";");
+        graphics.setForegroundColor(TextColor.ANSI.MAGENTA);
+        graphics.putString(new TerminalPosition(4,8), ";");
+        graphics.setForegroundColor(TextColor.ANSI.BLUE_BRIGHT);
+        graphics.putString(new TerminalPosition(2,9), ";");
+        graphics.setForegroundColor(TextColor.ANSI.BLACK);
+        graphics.putString(new TerminalPosition(8,9), ";");
+        */
+
         screen.refresh();
     }
 }
