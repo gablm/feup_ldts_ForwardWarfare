9af02da54aaaafa7edd5c1f28dc14a7e4fba3857
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2023-11-24 19:14:03 +0000

    new map

--- ForwardWarfare/src/main/java/com/ldts/ForwardWarfare/Game.java
+++ ForwardWarfare/src/main/java/com/ldts/ForwardWarfare/Game.java
@@ -41,123 +41,12 @@ public class Game {
     }
 
     private void DrawTiles(TextGraphics graphics) {
-        List<Element> elementList = new ArrayList<>();
-
-        elementList.add(new AntiAirTank(new Position(5, 5)));
-        elementList.add(new LightPerson(new Position(12, 7)));
-        elementList.add(new HeavyTank(new Position(3, 4)));
-        /*boolean change = true;
-        for (int j = 0; j < 160; j += 16) {
-            change = !change;
-            for (int i = 0; i < 256; i += 16) {
-                graphics.setBackgroundColor(change ? TextColor.ANSI.CYAN : TextColor.ANSI.YELLOW);
-                change = !change;
-                graphics.drawRectangle(new TerminalPosition(i, j), new TerminalSize(16, 16), ' ');
-            }
-        }*/
-        /*
-        graphics.setForegroundColor(TextColor.Factory.fromString("#FFFFFF"));
-
-        graphics.putString(new TerminalPosition(0,0), "Tanks $#+ ");
-        graphics.putString(new TerminalPosition(0,1), "Person (_@ ");
-        graphics.putString(new TerminalPosition(0,2), "Plane %& ");
-        graphics.putString(new TerminalPosition(0,3), "Helly [ ");
-        graphics.putString(new TerminalPosition(0,4), "Boat <' ");
-        graphics.putString(new TerminalPosition(0,5), "Submarine = ");
-        graphics.putString(new TerminalPosition(0,6), "Tiles~}{]|");
-        graphics.putString(new TerminalPosition(0,7), "Borders ^* ");
-        graphics.putString(new TerminalPosition(0,8), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-        graphics.putString(new TerminalPosition(0,9), "abcdefghijklmnop");
-        graphics.putString(new TerminalPosition(0,10),"Fac ;>`)/");
-        //graphics.putString(new TerminalPosition(0,10), ".,!?-:");*/
-
-        /*
-        graphics.setBackgroundColor(TextColor.ANSI.BLUE);
-        graphics.putString(new TerminalPosition(0,0), "~~~~~~~~~~~");
-        graphics.putString(new TerminalPosition(0,1), "~");
-        graphics.putString(new TerminalPosition(10,1), "~");
-        graphics.putString(new TerminalPosition(0,2), "~");
-        graphics.putString(new TerminalPosition(7,2), "~~~~");
-        graphics.putString(new TerminalPosition(0,3), "~~~~");
-        graphics.putString(new TerminalPosition(10,3), "~");
-        graphics.putString(new TerminalPosition(0,4), "~");
-        graphics.putString(new TerminalPosition(10,4), "~");
-        graphics.putString(new TerminalPosition(0,5), "~");
-        graphics.putString(new TerminalPosition(10,5), "~");
-        graphics.putString(new TerminalPosition(0,6), "~");
-        graphics.putString(new TerminalPosition(8,6), "~~~");
-        graphics.putString(new TerminalPosition(0,7), "~~~");
-        graphics.putString(new TerminalPosition(10,7), "~");
-        graphics.putString(new TerminalPosition(0,8), "~");
-        graphics.putString(new TerminalPosition(10,8), "~");
-        graphics.putString(new TerminalPosition(0,9), "~");
-        graphics.putString(new TerminalPosition(10,9), "~");
-        graphics.putString(new TerminalPosition(0,10), "~~~~~~~~~~~");
-        graphics.setForegroundColor(TextColor.ANSI.CYAN);
-        graphics.putString(new TerminalPosition(4,3), ";");
-        graphics.setForegroundColor(TextColor.ANSI.WHITE);
-        graphics.setBackgroundColor(new TextColor.RGB(128,128,128));
-        graphics.putString(new TerminalPosition(1,5), "}}}}");
-        graphics.putString(new TerminalPosition(6,5), "}}}}");
-        graphics.setBackgroundColor(TextColor.ANSI.GREEN);
-        graphics.putString(new TerminalPosition(1,1), "|");
-        graphics.putString(new TerminalPosition(4,1), "|||");
-        graphics.putString(new TerminalPosition(8,1), "||");
-        graphics.putString(new TerminalPosition(1,2), "||||||");
-        graphics.putString(new TerminalPosition(5,3), "|||");
-        graphics.putString(new TerminalPosition(9,3), "|");
-        graphics.putString(new TerminalPosition(1,4), "||");
-        graphics.putString(new TerminalPosition(4,4), "||||||");
-        graphics.putString(new TerminalPosition(5,5), "|");
-        graphics.putString(new TerminalPosition(1,6), "|");
-        graphics.putString(new TerminalPosition(3,6), "|||||");
-        graphics.putString(new TerminalPosition(3,7), "|||||||");
-        graphics.putString(new TerminalPosition(1,8), "|||");
-        graphics.putString(new TerminalPosition(5,8), "|||||");
-        graphics.putString(new TerminalPosition(1,9), "|");
-        graphics.putString(new TerminalPosition(3,9), "|||||");
-        graphics.putString(new TerminalPosition(9,9), "|");
-        graphics.setForegroundColor(TextColor.ANSI.BLACK);
-        graphics.putString(new TerminalPosition(3,1), "#");
-        graphics.putString(new TerminalPosition(2,1), ";");
-        graphics.setForegroundColor(TextColor.ANSI.BLUE_BRIGHT);
-        graphics.putString(new TerminalPosition(7,1), ";");
-        graphics.setForegroundColor(TextColor.ANSI.MAGENTA);
-        graphics.putString(new TerminalPosition(8,3), ";");
-        graphics.setForegroundColor(TextColor.ANSI.YELLOW);
-        graphics.putString(new TerminalPosition(3,4), ";");
-        graphics.setForegroundColor(TextColor.ANSI.YELLOW);
-        graphics.putString(new TerminalPosition(2,6), ";");
-        graphics.setForegroundColor(TextColor.ANSI.MAGENTA);
-        graphics.putString(new TerminalPosition(4,8), ";");
-        graphics.setForegroundColor(TextColor.ANSI.BLUE_BRIGHT);
-        graphics.putString(new TerminalPosition(2,9), ";");
-        graphics.setForegroundColor(TextColor.ANSI.BLACK);
-        graphics.putString(new TerminalPosition(8,9), ";");
-        */
-
-
-        elementList.add(new Florest(new Position(0,0)));
-        elementList.add(new Montain_land(new Position(0,1)));
-        elementList.add(new MountainWater(new Position(1,1)));
-        elementList.add(new Water(new Position(1,0),null));
-        elementList.add(new Water(new Position(2,0),null));
-        elementList.add(new Fields(new Position(0,2),null));
-        elementList.add(new Water(new Position(1,2),new Port()));
-        elementList.add(new Water(new Position(1,3),new OilPump()));
-        // elementList.add(new Fields(new Position(0,3),new Base()));
-        // elementList.add(new Fields(new Position(0,4),new Base()));
-        elementList.add(new Fields(new Position(0,5),new Factory()));
-        elementList.add(new Fields(new Position(0,6),new Airport()));
-        elementList.add(new Fields(new Position(0,7),new OilPump()));
-
-
-
-
-
-        for (Element element : elementList) {
+        Map map = new Map("0.fw");
+        for (Element element : map.getElements()) {
             if (element instanceof Playable)
-                ((Playable)element).draw(graphics, TextColor.ANSI.BLUE);
+                element.draw(graphics, TextColor.ANSI.BLUE);
+            else
+                element.draw(graphics, null);
         }
     }
 }
--- ForwardWarfare/src/main/java/com/ldts/ForwardWarfare/Map.java
+++ ForwardWarfare/src/main/java/com/ldts/ForwardWarfare/Map.java
@@ -7,14 +7,12 @@ import com.ldts.ForwardWarfare.Element.Position;
 import com.ldts.ForwardWarfare.Element.Tile.*;
 
 import java.io.File;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Map {
-    private List<Element> map;
-    private List<Position> spawnPoints;
-    private List<Element> inicialFactory;
+    private List<Element> map = new ArrayList<>();
+    private Element inicialFactory1;
+    private Element inicialFactory2;
 
     public Map() {
         readMap(null);
@@ -22,13 +20,18 @@ public class Map {
     public Map(String filePath) {
         readMap(filePath);
     }
-
+    public List<Element> getElements() {
+        return map;
+    }
+    public List<Element> getInitialFactories() {
+        return Arrays.asList(inicialFactory1, inicialFactory2);
+    }
     private void readMap(String filePath) {
         try {
             Random random = new Random();
             Scanner scanner;
             if (filePath != null)
-                scanner = new Scanner(new File("src/main/resources/maps/%d.fw".formatted(random.nextInt(3) + 1)));
+                scanner = new Scanner(new File("src/main/resources/maps/" + filePath));
             else
                 scanner = new Scanner(new File("src/main/resources/maps/%d.fw".formatted(random.nextInt(3) + 1)));
             scanner.useDelimiter("\n");
@@ -37,14 +40,14 @@ public class Map {
                 Scanner line = new Scanner(scanner.nextLine());
                 x = 0;
                 while (line.hasNext()) {
-                    Integer id = line.nextInt();
+                    int id = line.nextInt();
                     Position pos = new Position(x,y);
                     switch (id) {
                         case 1:
                             map.add(new Florest(pos));
                             break;
                         case 2:
-                            map.add(new Montain_land(pos));
+                            map.add(new MountainLand(pos));
                             break;
                         case 3:
                             map.add(new MountainWater(pos));
@@ -63,23 +66,40 @@ public class Map {
                             map.add(new Fields(pos, new Airport()));
                             break;
                         case 22:
-                            map.add(new Fields(pos, new Factory()));
-                            break;
-                        case 23:
                             map.add(new Fields(pos, new OilPump()));
                             break;
-                        case 24:
-                        case -24:
-                            map.add(new Fields(pos, new Base(id > 0 ? TextColor.ANSI.GREEN : TextColor.ANSI.RED)));
+                        case 27:
+                        case 28:
+                        case 29:
+                            Element iniField = new Fields(pos, new Factory());
+                            if (id == 28)
+                                if (inicialFactory1 == null)
+                                    inicialFactory1 = iniField;
+                                else
+                                    throw new IllegalArgumentException("A player cannot start with more than one factory");
+                            if (id == 29)
+                                if (inicialFactory2 == null)
+                                    inicialFactory2 = iniField;
+                                else
+                                    throw new IllegalArgumentException("A player cannot start with more than one factory");
+                            map.add(iniField);
+                            break;
+                        case 31:
+                        case 32:
+                            map.add(new Fields(pos, new Base(id == 31 ? TextColor.ANSI.YELLOW : TextColor.ANSI.RED)));
                             break;
                         default:
-                            throw new IllegalArgumentException();
+                            throw new IllegalArgumentException("There is an invalid number in the map: %d".formatted(id));
                     }
                     x++;
                 }
+                if (x != 15)
+                    throw new IllegalArgumentException("Invalid line size: %d".formatted(x));
                 y++;
             }
             scanner.close();
+            if (map.size() != 150)
+                throw new IllegalArgumentException("The map should be 15 x 10.");
         } catch (Exception e) {
             e.printStackTrace();
         }
