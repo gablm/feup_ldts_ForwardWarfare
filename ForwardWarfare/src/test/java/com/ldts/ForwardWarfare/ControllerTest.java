9b62da1ffb63d8ce020ec86dde53d65211ea6335
renatasimao04 <renata.simao2012@gmail.com>
2023-12-05 20:09:10 +0000

    Update ControllerTest

--- ForwardWarfare/src/test/java/com/ldts/ForwardWarfare/ControllerTest.java
+++ ForwardWarfare/src/test/java/com/ldts/ForwardWarfare/ControllerTest.java
@@ -1,10 +1,7 @@
 package com.ldts.ForwardWarfare;
 
 import com.googlecode.lanterna.TextColor;
-import com.ldts.ForwardWarfare.Controller.Bot;
-import com.ldts.ForwardWarfare.Controller.Controller;
-import com.ldts.ForwardWarfare.Controller.InvalidControllerException;
-import com.ldts.ForwardWarfare.Controller.Player;
+import com.ldts.ForwardWarfare.Controller.*;
 import com.ldts.ForwardWarfare.Element.Element;
 import com.ldts.ForwardWarfare.Element.Facility.Base;
 import com.ldts.ForwardWarfare.Element.Facility.Facility;
@@ -12,12 +9,17 @@ import com.ldts.ForwardWarfare.Element.Facility.Factory;
 import com.ldts.ForwardWarfare.Element.Playable.Ground.LightTank;
 import com.ldts.ForwardWarfare.Element.Playable.Playable;
 import com.ldts.ForwardWarfare.Element.Position;
+import com.ldts.ForwardWarfare.Element.Tile.Border;
 import com.ldts.ForwardWarfare.Element.Tile.Fields;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ControllerTest {
 
@@ -30,8 +32,8 @@ public class ControllerTest {
             new Bot(null, TextColor.ANSI.GREEN_BRIGHT);
         });
 
-        Assertions.assertEquals("Invalid initial Factory and Base", playerEx.getMessage());
-        Assertions.assertEquals("Invalid initial Factory and Base", botEx.getMessage());
+        assertEquals("Invalid initial Factory and Base", playerEx.getMessage());
+        assertEquals("Invalid initial Factory and Base", botEx.getMessage());
     }
 
     @Test
@@ -44,8 +46,8 @@ public class ControllerTest {
             new Bot(elements, TextColor.ANSI.GREEN_BRIGHT);
         });
 
-        Assertions.assertEquals("Invalid initial Factory and Base", playerEx.getMessage());
-        Assertions.assertEquals("Invalid initial Factory and Base", botEx.getMessage());
+        assertEquals("Invalid initial Factory and Base", playerEx.getMessage());
+        assertEquals("Invalid initial Factory and Base", botEx.getMessage());
     }
 
     @Test
@@ -56,9 +58,9 @@ public class ControllerTest {
 
         Controller player = new Player(elements, TextColor.ANSI.GREEN_BRIGHT);
 
-        Assertions.assertEquals(100, player.getCoins());
+        assertEquals(100, player.getCoins());
         Assertions.assertSame(player.getBase(), base);
-        Assertions.assertEquals(1, player.getFacilities().size());
+        assertEquals(1, player.getFacilities().size());
         Assertions.assertSame(factory, player.getFacilities().get(0));
         Assertions.assertTrue(player.getTroops().isEmpty());
     }
@@ -71,12 +73,12 @@ public class ControllerTest {
 
         Controller player = new Player(elements, TextColor.ANSI.GREEN_BRIGHT);
 
-        Assertions.assertEquals(100, player.getCoins());
+        assertEquals(100, player.getCoins());
         Playable troop = new LightTank(new Position(1, 1));
         boolean result = player.buy(troop, 100);
         Assertions.assertTrue(result);
-        Assertions.assertEquals(0, player.getCoins());
-        Assertions.assertEquals(1, player.getTroops().size());
+        assertEquals(0, player.getCoins());
+        assertEquals(1, player.getTroops().size());
         Assertions.assertSame(troop, player.getTroops().get(0));
     }
 
@@ -88,12 +90,12 @@ public class ControllerTest {
 
         Controller player = new Player(elements, TextColor.ANSI.GREEN_BRIGHT);
 
-        Assertions.assertEquals(100, player.getCoins());
+        assertEquals(100, player.getCoins());
         Playable troop = new LightTank(new Position(1, 1));
         boolean result = player.buy(troop, 50);
         Assertions.assertTrue(result);
-        Assertions.assertEquals(50, player.getCoins());
-        Assertions.assertEquals(1, player.getTroops().size());
+        assertEquals(50, player.getCoins());
+        assertEquals(1, player.getTroops().size());
         Assertions.assertSame(troop, player.getTroops().get(0));
     }
 
@@ -105,11 +107,81 @@ public class ControllerTest {
 
         Controller player = new Player(elements, TextColor.ANSI.GREEN_BRIGHT);
 
-        Assertions.assertEquals(100, player.getCoins());
+        assertEquals(100, player.getCoins());
         Playable troop = new LightTank(new Position(1, 1));
         boolean result = player.buy(troop, 120);
-        Assertions.assertFalse(result);
-        Assertions.assertEquals(100, player.getCoins());
+        assertFalse(result);
+        assertEquals(100, player.getCoins());
         Assertions.assertTrue(player.getTroops().isEmpty());
+    }
+
+    @Test
+    public void testEndRound() {
+        Controller yourInstance;
+        yourInstance.endRound();
+
+        assertFalse(yourInstance.canPlay());
+        assertNull(yourInstance.getSelection1());
+        assertNull(yourInstance.getSelection2());
+    }
+
+    @Test
+    public void testGetSetSelection1() {
+        Mockito Mockito;
+        Border borderMock = Mockito.mock(Border.class);
+        Controller yourInstance;
+        yourInstance.setSelection1(borderMock);
+
+        assertEquals(borderMock, yourInstance.getSelection1());
+    }
+
+    @Test
+    public void testGetSetSelection2() {
+        Border borderMock = Mockito.mock(Border.class);
+        Controller yourInstance;
+        yourInstance.setSelection2(borderMock);
+
+        assertEquals(borderMock, yourInstance.getSelection2());
+    }
+
+    @Test
+    public void testDraw() {
+        TextGraphics graphicsMock = Mockito.mock(TextGraphics.class);
+        ControllerBase yourInstance;
+        yourInstance.draw(graphicsMock);
+
+        // Verify if draw method is invoked for each element in troops and facilities
+        Mockito.verify(graphicsMock, Mockito.times(yourInstance.getTroops().size() + yourInstance.getFacilities().size())).draw(Mockito.any(Element.class));
+    }
+
+    @Test
+    public void testDrawBorder() {
+        TextGraphics graphicsMock = Mockito.mock(TextGraphics.class);
+
+        Border selection1Mock = Mockito.mock(Border.class);
+        Border selection2Mock = Mockito.mock(Border.class);
+
+        yourInstance.setSelection1(selection1Mock);
+        yourInstance.setSelection2(selection2Mock);
+
+        yourInstance.drawBorder(graphicsMock);
+
+        Mockito.verify(selection1Mock, Mockito.times(1)).draw(graphicsMock);
+        Mockito.verify(selection2Mock, Mockito.times(1)).draw(graphicsMock);
+    }
+
+    @Test
+    public void testResetRound() {
+        int initialCoins = yourInstance.getCoins();
+
+        yourInstance.resetRound();
+
+        assertTrue(yourInstance.canPlay());
+        assertEquals(initialCoins + 100 + 30 * yourInstance.getFacilities().size(), yourInstance.getCoins());
+    }
+
+    @Test
+    public void testCanPlay() {
+        assertTrue(yourInstance.canPlay()); // Assuming initially canPlay is true
     }
 }
