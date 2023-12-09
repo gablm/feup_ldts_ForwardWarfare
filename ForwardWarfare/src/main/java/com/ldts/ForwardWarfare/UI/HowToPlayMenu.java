b848264068db5201fd40effc0cd9500a0b3f5576
renatasimao04 <renata.simao2012@gmail.com>
2023-12-09 15:58:55 +0000

    Update

--- ForwardWarfare/src/main/java/com/ldts/ForwardWarfare/UI/HowToPlayMenu.java
+++ ForwardWarfare/src/main/java/com/ldts/ForwardWarfare/UI/HowToPlayMenu.java
@@ -17,6 +17,7 @@ import java.net.URISyntaxException;
 
 public class HowToPlayMenu extends UI{
     private UiStates startgame;
+    private Position position;
 
     public HowToPlayMenu() {
         super(new TerminalSize(75,50),15);
@@ -33,14 +34,19 @@ public class HowToPlayMenu extends UI{
     public void draw() throws IOException {
         screen.clear();
         TextGraphics graphics = screen.newTextGraphics();
-        graphics.setBackgroundColor(new TextColor.RGB(117,200,4));
+        graphics.setBackgroundColor(new TextColor.RGB(117, 200, 4));
         graphics.enableModifiers(SGR.BOLD);
-        graphics.fillRectangle(new TerminalPosition(0,0),new TerminalSize(75,50),' ');
-        for(Component comp :listComponents)
-        {
+        graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(75, 50), ' ');
+        for (Component comp : listComponents) {
             comp.draw(graphics);
         }
         screen.refresh();
+        int x;
+        int y;
+        TextGraphics textGraphics = graphics.putString(new TerminalPosition(position.getX(), position.getY()), "\"FowardWarfare is our recreation of the game called Advance Wars. It’s a series of turn-based strategy games developed by Intelligent Systems and published by Nintendo." +
+                " The game was first released for the Game Boy Advance and features tactical battles between armies on a grid-based map. It offers challenging gameplay where players command various military units such as infantry, tanks, aircraft, and ships, each with unique abilities and characteristics. " +
+                "The objective is to defeat the enemy army, capture bases, and control facilities to obtain resources and additional units. This said, it also allows us to build troops in different places, like airports, factories, ports for example. To obtain cash we have a base income, but also, every time we . On other side, if our opponent capture our base we lose the game.\"));\n");
+
     }
 
     @Override
