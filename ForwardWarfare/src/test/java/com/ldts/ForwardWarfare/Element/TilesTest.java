69bca1c23f104ad0f40b273a91bbeaeb685b45e3
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2023-12-11 12:28:43 +0000

    Tile testing

--- ForwardWarfare/src/test/java/com/ldts/ForwardWarfare/Element/TilesTest.java
+++ ForwardWarfare/src/test/java/com/ldts/ForwardWarfare/Element/TilesTest.java
@@ -3,9 +3,7 @@ package com.ldts.ForwardWarfare.Element;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.ldts.ForwardWarfare.Element.Facility.Facility;
-import com.ldts.ForwardWarfare.Element.Tile.Border;
-import com.ldts.ForwardWarfare.Element.Tile.Fields;
-import com.ldts.ForwardWarfare.Element.Tile.Water;
+import com.ldts.ForwardWarfare.Element.Tile.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -113,6 +111,58 @@ public class TilesTest {
 
     @Test
     public void FlorestTest() {
+        Position position = new Position(0, 0);
+        Florest florest = new Florest(position);
 
+        Assertions.assertNull(florest.getFacility());
+        Assertions.assertFalse(florest.noCollision());
+
+        TextGraphics graphics = Mockito.mock(TextGraphics.class);
+
+        florest.draw(graphics);
+
+        Mockito.verify(graphics).setForegroundColor(new TextColor.RGB(0,102,51));
+        Mockito.verify(graphics).setBackgroundColor(new TextColor.RGB(113,199,0));
+        Mockito.verify(graphics).putString(position.toTPos(), "{");
+
+        Assertions.assertEquals(new TextColor.RGB(113,199,0), florest.getColor());
+    }
+
+    @Test
+    public void MountainLandTest() {
+        Position position = new Position(0, 0);
+        MountainLand mountainLand = new MountainLand(position);
+
+        Assertions.assertNull(mountainLand.getFacility());
+        Assertions.assertFalse(mountainLand.noCollision());
+
+        TextGraphics graphics = Mockito.mock(TextGraphics.class);
+
+        mountainLand.draw(graphics);
+
+        Mockito.verify(graphics).setForegroundColor(new TextColor.RGB(57,45,45));
+        Mockito.verify(graphics).setBackgroundColor(new TextColor.RGB(113,199,0));
+        Mockito.verify(graphics).putString(position.toTPos(), "}");
+
+        Assertions.assertEquals(new TextColor.RGB(113,199,0), mountainLand.getColor());
+    }
+
+    @Test
+    public void MountainWaterTest() {
+        Position position = new Position(0, 0);
+        MountainWater mountainWater = new MountainWater(position);
+
+        Assertions.assertNull(mountainWater.getFacility());
+        Assertions.assertFalse(mountainWater.noCollision());
+
+        TextGraphics graphics = Mockito.mock(TextGraphics.class);
+
+        mountainWater.draw(graphics);
+
+        Mockito.verify(graphics).setForegroundColor(new TextColor.RGB(160,160,160));
+        Mockito.verify(graphics).setBackgroundColor(new TextColor.RGB(0,124,206));
+        Mockito.verify(graphics).putString(position.toTPos(), "]");
+
+        Assertions.assertEquals(new TextColor.RGB(0,124,206), mountainWater.getColor());
     }
 }
