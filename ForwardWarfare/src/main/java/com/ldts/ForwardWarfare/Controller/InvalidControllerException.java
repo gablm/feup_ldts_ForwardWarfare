8857a6a01747a9c17f2b3dfc1420a890c8bac95e
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2023-12-18 19:01:56 +0000

    Remove unused code

--- ForwardWarfare/src/main/java/com/ldts/ForwardWarfare/Controller/InvalidControllerException.java
+++ ForwardWarfare/src/main/java/com/ldts/ForwardWarfare/Controller/InvalidControllerException.java
@@ -1,7 +1,6 @@
 package com.ldts.ForwardWarfare.Controller;
 
 public class InvalidControllerException extends Exception {
-    public InvalidControllerException() {}
     public InvalidControllerException(String message) {
         super(message);
     }
--- ForwardWarfare/src/main/java/com/ldts/ForwardWarfare/Map/MapParseException.java
+++ ForwardWarfare/src/main/java/com/ldts/ForwardWarfare/Map/MapParseException.java
@@ -1,8 +1,6 @@
 package com.ldts.ForwardWarfare.Map;
 
 public class MapParseException extends Exception {
-    public MapParseException() {}
-
     public MapParseException(String message)
     {
         super(message);
