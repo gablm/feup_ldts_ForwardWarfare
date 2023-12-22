63984cbf4b4934ce75dcab652b161be390911f11
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2023-12-22 00:20:58 +0000

    buyHighest from AutomaticPlay testing

--- ForwardWarfare/src/test/java/com/ldts/ForwardWarfare/State/AutomaticPlayTest.java
+++ ForwardWarfare/src/test/java/com/ldts/ForwardWarfare/State/AutomaticPlayTest.java
@@ -5,22 +5,32 @@ import com.ldts.ForwardWarfare.Controller.Controller;
 import com.ldts.ForwardWarfare.Element.Element;
 import com.ldts.ForwardWarfare.Element.Facility.Base;
 import com.ldts.ForwardWarfare.Element.Facility.Facility;
+import com.ldts.ForwardWarfare.Element.Facility.OilPump;
+import com.ldts.ForwardWarfare.Element.Playable.Air.FighterPlane;
+import com.ldts.ForwardWarfare.Element.Playable.Ground.HeavyPerson;
 import com.ldts.ForwardWarfare.Element.Playable.Ground.HeavyTank;
+import com.ldts.ForwardWarfare.Element.Playable.Playable;
 import com.ldts.ForwardWarfare.Element.Position;
 import com.ldts.ForwardWarfare.Element.Tile.Fields;
+import com.ldts.ForwardWarfare.Element.Tile.MountainLand;
 import com.ldts.ForwardWarfare.Element.Tile.Tile;
 import com.ldts.ForwardWarfare.Map.Map;
 import com.ldts.ForwardWarfare.Map.MapParseException;
 import com.ldts.ForwardWarfare.State.States.Automatic.AutomaticPlayState;
 import com.ldts.ForwardWarfare.State.States.EndGameState;
+import com.ldts.ForwardWarfare.State.States.Player.SpawnTroopState;
 import com.ldts.ForwardWarfare.State.States.StartRoundState;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mockito;
 
 import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -123,5 +133,228 @@ public class AutomaticPlayTest {
         List<Position> elements = state.canMove(pos1, pos2, new HeavyTank(pos1));
 
         Assertions.assertEquals(8, elements.size());
+    }
+
+    @Test
+    public void BuyHighestNoCoinsTest() throws FileNotFoundException, MapParseException, URISyntaxException {
+        map = new Map("tests/allGrass.fw");
+
+        List<Element> troops = List.of(new HeavyTank(new Position(5, 5)));
+        Mockito.when(p1.getTroops()).thenReturn(troops);
+        Mockito.when(p1.getBase())
+                .thenReturn(new Fields(new Position(0, 0), new Base()));
+        Mockito.when(p2.getTroops()).thenReturn(
+                List.of(new HeavyTank(new Position(5, 5))));
+        Mockito.when(p2.getBase())
+                .thenReturn(new Fields(new Position(0, 0), new Base()));
+        Mockito.when(p1.getCoins()).thenReturn(0);
+
+        AutomaticPlayState state = new AutomaticPlayState(p1, p2, map);
+        state.buyHighest(0, new Position(5, 5));
+
+        Mockito.verify(p1, Mockito.never()).buy(Mockito.any(), Mockito.anyInt());
+    }
+
+    @Test
+    public void BuyHighestTest() throws FileNotFoundException, MapParseException, URISyntaxException {
+        map = new Map("tests/allGrass.fw");
+
+        List<Element> troops = new ArrayList<>();
+        troops.add(new HeavyTank(new Position(5, 5)));
+        Mockito.when(p1.getTroops()).thenReturn(troops);
+        Mockito.when(p1.getBase())
+                .thenReturn(new Fields(new Position(0, 0), new Base()));
+        Mockito.when(p2.getTroops()).thenReturn(
+                List.of(new HeavyTank(new Position(5, 5))));
+        Mockito.when(p2.getBase())
+                .thenReturn(new Fields(new Position(0, 0), new Base()));
+        Mockito.when(p1.getCoins()).thenReturn(100);
+
+        AutomaticPlayState state = new AutomaticPlayState(p1, p2, map);
+        state.buyHighest(0, new Position(5, 5));
+
+        Mockito.verify(p1).buy(Mockito.any(), Mockito.anyInt());
+    }
+
+    public void BuyHighestAroundFormat(List<Position> pos) throws FileNotFoundException, MapParseException, URISyntaxException {
+        map = new Map("tests/allGrass.fw");
+
+        Mockito.when(p1.getTroops()).thenReturn(new ArrayList<>());
+        Mockito.when(p1.getBase())
+                .thenReturn(new Fields(new Position(0, 0), new Base()));
+        Mockito.when(p2.getTroops()).thenReturn(new ArrayList<>());
+        Mockito.when(p2.getBase())
+                .thenReturn(new Fields(new Position(0, 0), new Base()));
+
+        AutomaticPlayState state = new AutomaticPlayState(p1, p2, map);
+
+        Mockito.reset(p1);
+        List<Element> troopsP1 = Arrays.asList(
+                new HeavyTank(pos.get(0)),
+                new HeavyPerson(pos.get(1)),
+                new HeavyPerson(pos.get(2)),
+                new HeavyPerson(pos.get(3))
+        );
+        Mockito.when(p1.getTroops()).thenReturn(troopsP1);
+        List<Element> troopsP2 = Arrays.asList(
+                new HeavyTank(pos.get(4)),
+                new HeavyPerson(pos.get(5)),
+                new HeavyPerson(pos.get(6))
+        );
+        Mockito.when(p2.getTroops()).thenReturn(troopsP2);
+
+        Mockito.when(p1.getCoins()).thenReturn(10000);
+
+        state.buyHighest(0, new Position(5, 5));
+
+        Mockito.verify(p1).buy(Mockito.any(), Mockito.anyInt());
+    }
+
+    @Test
+    public void Around0Test() throws FileNotFoundException, MapParseException, URISyntaxException {
+        List<Position> pos = Arrays.asList(
+                new Position(4, 4),
+                new Position(4, 5),
+                new Position(4, 6),
+                new Position(5, 4),
+                new Position(6, 4),
+                new Position(5, 6),
+                new Position(6, 6)
+        );
+        BuyHighestAroundFormat(pos);
+    }
+
+    @Test
+    public void Around1Test() throws FileNotFoundException, MapParseException, URISyntaxException {
+        List<Position> pos = Arrays.asList(
+                new Position(4, 4),
+                new Position(4, 5),
+                new Position(4, 6),
+                new Position(5, 4),
+                new Position(6, 4),
+                new Position(5, 6),
+                new Position(6, 5)
+        );
+        BuyHighestAroundFormat(pos);
+    }
+
+    @Test
+    public void Around2Test() throws FileNotFoundException, MapParseException, URISyntaxException {
+        List<Position> pos = Arrays.asList(
+                new Position(4, 4),
+                new Position(4, 5),
+                new Position(4, 6),
+                new Position(5, 4),
+                new Position(6, 4),
+                new Position(6, 5),
+                new Position(6, 6)
+        );
+        BuyHighestAroundFormat(pos);
+    }
+
+    @Test
+    public void Around3Test() throws FileNotFoundException, MapParseException, URISyntaxException {
+        List<Position> pos = Arrays.asList(
+                new Position(4, 4),
+                new Position(4, 5),
+                new Position(5, 4),
+                new Position(5, 6),
+                new Position(6, 4),
+                new Position(6, 5),
+                new Position(6, 6)
+        );
+        BuyHighestAroundFormat(pos);
+    }
+
+    @Test
+    public void Around4Test() throws FileNotFoundException, MapParseException, URISyntaxException {
+        List<Position> pos = Arrays.asList(
+                new Position(4, 4),
+                new Position(4, 6),
+                new Position(5, 4),
+                new Position(5, 6),
+                new Position(6, 4),
+                new Position(6, 5),
+                new Position(6, 6)
+        );
+        BuyHighestAroundFormat(pos);
+    }
+
+    @Test
+    public void Around5Test() throws FileNotFoundException, MapParseException, URISyntaxException {
+        List<Position> pos = Arrays.asList(
+                new Position(4, 5),
+                new Position(4, 6),
+                new Position(5, 4),
+                new Position(5, 6),
+                new Position(6, 4),
+                new Position(6, 5),
+                new Position(6, 6)
+        );
+        BuyHighestAroundFormat(pos);
+    }
+
+    @Test
+    public void Around6Test() throws FileNotFoundException, MapParseException, URISyntaxException {
+        List<Position> pos = Arrays.asList(
+                new Position(4, 4),
+                new Position(4, 5),
+                new Position(4, 6),
+                new Position(5, 6),
+                new Position(6, 4),
+                new Position(6, 5),
+                new Position(6, 6)
+        );
+        BuyHighestAroundFormat(pos);
+    }
+
+    @Test
+    public void Around7Test() throws FileNotFoundException, MapParseException, URISyntaxException {
+        List<Position> pos = Arrays.asList(
+                new Position(4, 4),
+                new Position(4, 5),
+                new Position(4, 6),
+                new Position(5, 4),
+                new Position(5, 6),
+                new Position(6, 5),
+                new Position(6, 6)
+        );
+        BuyHighestAroundFormat(pos);
+    }
+
+    @Test
+    public void BuyHighestAroundTest() throws FileNotFoundException, MapParseException, URISyntaxException {
+        map = new Map("tests/allGrass.fw");
+
+        Mockito.when(p1.getTroops()).thenReturn(new ArrayList<>());
+        Mockito.when(p1.getBase())
+                .thenReturn(new Fields(new Position(0, 0), new Base()));
+        Mockito.when(p2.getTroops()).thenReturn(new ArrayList<>());
+        Mockito.when(p2.getBase())
+                .thenReturn(new Fields(new Position(0, 0), new Base()));
+
+        AutomaticPlayState state = new AutomaticPlayState(p1, p2, map);
+
+        Mockito.reset(p1);
+        List<Element> troopsP1 = Arrays.asList(
+                new HeavyTank(new Position(4, 4)),
+                new HeavyPerson(new Position(4, 5)),
+                new HeavyPerson(new Position(4, 6)),
+                new HeavyPerson(new Position(5, 4)),
+                new HeavyPerson(new Position(5, 6))
+        );
+        Mockito.when(p1.getTroops()).thenReturn(troopsP1);
+        List<Element> troopsP2 = Arrays.asList(
+                new HeavyTank(new Position(6, 4)),
+                new HeavyPerson(new Position(6, 5)),
+                new HeavyPerson(new Position(6, 6))
+        );
+        Mockito.when(p2.getTroops()).thenReturn(troopsP2);
+
+        Mockito.when(p1.getCoins()).thenReturn(10000);
+
+        state.buyHighest(0, new Position(5, 5));
+
+        Mockito.verify(p1, Mockito.never()).buy(Mockito.any(), Mockito.anyInt());
     }
 }
